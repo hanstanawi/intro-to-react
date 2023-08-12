c6d90de46617e62e19fda4981fd9e7ff9b3d0c90
Hans Tanawi <hans.tanawi@outlook.com>
2023-08-12 13:19:33 +0800

    feat: add update and delete feature

--- src/components/tasks-list.jsx
+++ src/components/tasks-list.jsx
@@ -1,10 +1,19 @@
 import TaskItem from './task-item';
 
 export default function TasksList(props) {
+  const sortedTasks = props.tasks.sort((a, b) => {
+    return a.isDone === b.isDone ? 0 : b.isDone ? -1 : 1;
+  });
+
   return (
     <ul className='tasks-list'>
-      {props.tasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
+      {sortedTasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onUpdateTask={props.onUpdateTask}
+          onDeleteTask={props.onDeleteTask}
+        />
       ))}
     </ul>
   );
