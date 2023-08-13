5953229a43d56690d734a2364158a2b2eb8eee75
Hans Tanawi <hans.tanawi@outlook.com>
2023-08-13 22:13:34 +0800

    implement class component example

--- src/components/task-item.jsx
+++ src/components/task-item.jsx
@@ -1,9 +1,13 @@
-export default function TaskItem() {
-  return (
-    <article>
-      <h1>Learn React</h1>
-      <button>Update</button>
-      <button>Delete</button>
-    </article>
-  );
+import React from 'react';
+
+export default class TaskItem extends React.Component {
+  render() {
+    return (
+      <article>
+        <h1>Learn React</h1>
+        <button>Update</button>
+        <button>Delete</button>
+      </article>
+    );
+  }
 }
